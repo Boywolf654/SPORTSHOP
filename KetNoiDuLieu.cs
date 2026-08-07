87aea4fb413bc5a22ed9ae2b64db17db215ab9a6
Boywolf654 <108484715+Boywolf654@users.noreply.github.com>
2026-08-07 10:43:05 +0700

    Add files via upload

---
+++ KetNoiDuLieu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPORTSHOP
+{
+    public class KetNoiDuLieu
+    {
+        KetNoiDuLieu kn = new KetNoiDuLieu();
+        string strconn = "Data Source = DESKTOP - AOGCINN; InitialCatalog = ShopTheThao; IntegratedSecurity = True; TrustServerCertificate=True";
+        SqlConnection conn = null;
+        SqlDataAdapter da = null;
+        DataTable dt = null;
+        SqlCommand cmd = null;
+        SqlDataReader dr = null;
+        bool flag = true;
+        public static string TK = "";
+    }
+}
