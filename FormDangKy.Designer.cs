f86d5e11fbf67d9b83cd0529cb21d90622b904a0
Đức Đặng <duc12632006@gmail.com>
2026-08-14 10:28:51 +0700

    Thêm form Đăng Nhập

--- FormDangKy.Designer.cs
+++ FormDangKy.Designer.cs
@@ -28,12 +28,205 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lb_logo = new System.Windows.Forms.Label();
+            this.lb_TieuDe = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btn_DangKy = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.btn_DangKy);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lb_TieuDe);
+            this.panel1.Controls.Add(this.lb_logo);
+            this.panel1.Location = new System.Drawing.Point(641, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(319, 622);
+            this.panel1.TabIndex = 0;
+            // 
+            // lb_logo
+            // 
+            this.lb_logo.AutoSize = true;
+            this.lb_logo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_logo.ForeColor = System.Drawing.Color.Gold;
+            this.lb_logo.Location = new System.Drawing.Point(69, 9);
+            this.lb_logo.Name = "lb_logo";
+            this.lb_logo.Size = new System.Drawing.Size(174, 29);
+            this.lb_logo.TabIndex = 0;
+            this.lb_logo.Text = "SPORTSHOP";
+            this.lb_logo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb_TieuDe
+            // 
+            this.lb_TieuDe.AutoSize = true;
+            this.lb_TieuDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_TieuDe.ForeColor = System.Drawing.Color.Yellow;
+            this.lb_TieuDe.Location = new System.Drawing.Point(19, 59);
+            this.lb_TieuDe.Name = "lb_TieuDe";
+            this.lb_TieuDe.Size = new System.Drawing.Size(276, 29);
+            this.lb_TieuDe.TabIndex = 1;
+            this.lb_TieuDe.Text = "THÔNG TIN ĐĂNG KÝ";
+            this.lb_TieuDe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(20, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "HỌ VÀ TÊN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(20, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "USERNAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(20, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PASSWORD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(20, 352);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NHẬP LẠI PASSWORD";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(60, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(60, 234);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.ForeColor = System.Drawing.Color.Black;
+            this.textBox3.Location = new System.Drawing.Point(60, 313);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(196, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.ForeColor = System.Drawing.Color.Black;
+            this.textBox4.Location = new System.Drawing.Point(60, 393);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(196, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // btn_DangKy
+            // 
+            this.btn_DangKy.BackColor = System.Drawing.Color.Gold;
+            this.btn_DangKy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_DangKy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DangKy.ForeColor = System.Drawing.Color.Black;
+            this.btn_DangKy.Location = new System.Drawing.Point(47, 450);
+            this.btn_DangKy.Name = "btn_DangKy";
+            this.btn_DangKy.Size = new System.Drawing.Size(224, 80);
+            this.btn_DangKy.TabIndex = 5;
+            this.btn_DangKy.Text = "ĐĂNG KÝ";
+            this.btn_DangKy.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(24, 570);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(271, 20);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "ĐÃ CÓ TÀI KHOẢN? ĐĂNG NHẬP";
+            // 
+            // FormDangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::SPORTSHOP.Properties.Resources._1115ec25_ce99_469b_bd2b_8abc4a7915b6;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(972, 646);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormDangKy";
             this.Text = "FormDangKy";
+            this.Load += new System.EventHandler(this.FormDangKy_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lb_logo;
+        private System.Windows.Forms.Label lb_TieuDe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btn_DangKy;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
--- FormDangKy.cs
+++ FormDangKy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,20 @@ namespace SPORTSHOP
         public FormDangKy()
         {
             InitializeComponent();
+        }
+
+        private void FormDangKy_Load(object sender, EventArgs e)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int radius = 20;
+
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(btn_DangKy.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(btn_DangKy.Width - radius, btn_DangKy.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, btn_DangKy.Height - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+
+            btn_DangKy.Region = new Region(path);
         }
     }
 }
