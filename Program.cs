b822b303d20498f19b0b1b157125dbc007d13195
duypham <duy11025z123@gmail.com>
2026-09-03 08:00:19 +0700

    Update form nhap hang and SQL

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +22,7 @@ namespace SPORTSHOP
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmPhieuKho());
+            Application.Run(new FormNhapHang());
         }
 
         // Khai báo hàm API của Windows
@@ -29,3 +30,4 @@ namespace SPORTSHOP
         private static extern bool SetProcessDPIAware();
     }
 }
+```
