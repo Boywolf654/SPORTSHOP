8ded47b9c063512a14bb0cc6934ecac348ea4bbd
tuanledstn-cpu <tuanledstn@gmail.com>
2026-09-17 08:17:19 +0700

    Thêm form coupon và thanh toán

---
+++ 06_BanHang/coupon.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SPORTSHOP._06_BanHang
+{
+    partial class coupon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_giam = new System.Windows.Forms.TextBox();
+            this.txt_dontoithieu = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(212, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 53);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "ÁP DỤNG ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(101, 124);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(239, 31);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "SPORT10 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(56, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Chọn mã: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(176, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "🎟 MÃ GIẢM GIÁ";
+            // 
+            // txt_giam
+            // 
+            this.txt_giam.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_giam.Location = new System.Drawing.Point(101, 213);
+            this.txt_giam.Name = "txt_giam";
+            this.txt_giam.Size = new System.Drawing.Size(239, 30);
+            this.txt_giam.TabIndex = 12;
+            this.txt_giam.Text = "✓ Giảm 10%";
+            // 
+            // txt_dontoithieu
+            // 
+            this.txt_dontoithieu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_dontoithieu.Location = new System.Drawing.Point(101, 295);
+            this.txt_dontoithieu.Name = "txt_dontoithieu";
+            this.txt_dontoithieu.Size = new System.Drawing.Size(239, 30);
+            this.txt_dontoithieu.TabIndex = 13;
+            this.txt_dontoithieu.Text = "✓ Đơn tối thiểu 500.000đ";
+            // 
+            // coupon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 450);
+            this.Controls.Add(this.txt_dontoithieu);
+            this.Controls.Add(this.txt_giam);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "coupon";
+            this.Text = "coupon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_giam;
+        private System.Windows.Forms.TextBox txt_dontoithieu;
+    }
+}
---
+++ 06_BanHang/themcoupon.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SPORTSHOP._06_BanHang
+{
+    partial class themcoupon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_huy = new System.Windows.Forms.Button();
+            this.btn_them = new System.Windows.Forms.Button();
+            this.dt_ketthuc = new System.Windows.Forms.DateTimePicker();
+            this.dt_ngaybatdau = new System.Windows.Forms.DateTimePicker();
+            this.txt_dontoithieu = new System.Windows.Forms.TextBox();
+            this.txt_giatri = new System.Windows.Forms.TextBox();
+            this.txt_loai = new System.Windows.Forms.TextBox();
+            this.txt_ten = new System.Windows.Forms.TextBox();
+            this.txt_macoupon = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_huy
+            // 
+            this.btn_huy.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_huy.Location = new System.Drawing.Point(290, 673);
+            this.btn_huy.Name = "btn_huy";
+            this.btn_huy.Size = new System.Drawing.Size(120, 38);
+            this.btn_huy.TabIndex = 33;
+            this.btn_huy.Text = " HỦY  ";
+            this.btn_huy.UseVisualStyleBackColor = true;
+            // 
+            // btn_them
+            // 
+            this.btn_them.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_them.Location = new System.Drawing.Point(41, 673);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(190, 38);
+            this.btn_them.TabIndex = 32;
+            this.btn_them.Text = "THÊM COUPON ";
+            this.btn_them.UseVisualStyleBackColor = true;
+            // 
+            // dt_ketthuc
+            // 
+            this.dt_ketthuc.CalendarFont = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.dt_ketthuc.Location = new System.Drawing.Point(196, 563);
+            this.dt_ketthuc.Name = "dt_ketthuc";
+            this.dt_ketthuc.Size = new System.Drawing.Size(253, 22);
+            this.dt_ketthuc.TabIndex = 31;
+            // 
+            // dt_ngaybatdau
+            // 
+            this.dt_ngaybatdau.CalendarFont = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.dt_ngaybatdau.Location = new System.Drawing.Point(196, 488);
+            this.dt_ngaybatdau.Name = "dt_ngaybatdau";
+            this.dt_ngaybatdau.Size = new System.Drawing.Size(253, 22);
+            this.dt_ngaybatdau.TabIndex = 30;
+            // 
+            // txt_dontoithieu
+            // 
+            this.txt_dontoithieu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_dontoithieu.Location = new System.Drawing.Point(196, 412);
+            this.txt_dontoithieu.Name = "txt_dontoithieu";
+            this.txt_dontoithieu.Size = new System.Drawing.Size(272, 30);
+            this.txt_dontoithieu.TabIndex = 29;
+            // 
+            // txt_giatri
+            // 
+            this.txt_giatri.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_giatri.Location = new System.Drawing.Point(196, 339);
+            this.txt_giatri.Name = "txt_giatri";
+            this.txt_giatri.Size = new System.Drawing.Size(272, 30);
+            this.txt_giatri.TabIndex = 28;
+            // 
+            // txt_loai
+            // 
+            this.txt_loai.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_loai.Location = new System.Drawing.Point(196, 265);
+            this.txt_loai.Name = "txt_loai";
+            this.txt_loai.Size = new System.Drawing.Size(272, 30);
+            this.txt_loai.TabIndex = 27;
+            // 
+            // txt_ten
+            // 
+            this.txt_ten.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_ten.Location = new System.Drawing.Point(196, 188);
+            this.txt_ten.Name = "txt_ten";
+            this.txt_ten.Size = new System.Drawing.Size(272, 30);
+            this.txt_ten.TabIndex = 26;
+            // 
+            // txt_macoupon
+            // 
+            this.txt_macoupon.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_macoupon.Location = new System.Drawing.Point(196, 120);
+            this.txt_macoupon.Name = "txt_macoupon";
+            this.txt_macoupon.Size = new System.Drawing.Size(272, 30);
+            this.txt_macoupon.TabIndex = 25;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label8.Location = new System.Drawing.Point(17, 563);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(136, 23);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Ngày kết thúc:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.Location = new System.Drawing.Point(17, 487);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 23);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Ngày bắt đầu:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.Location = new System.Drawing.Point(17, 415);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 23);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Đơn tối thiểu:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(17, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 23);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Giá trị giảm: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(17, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Loại giảm:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(17, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 23);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Tên chương trình:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(17, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 23);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Mã coupon:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(133, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 32);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "MÃ GIẢM GIÁ";
+            // 
+            // themcoupon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 733);
+            this.Controls.Add(this.btn_huy);
+            this.Controls.Add(this.btn_them);
+            this.Controls.Add(this.dt_ketthuc);
+            this.Controls.Add(this.dt_ngaybatdau);
+            this.Controls.Add(this.txt_dontoithieu);
+            this.Controls.Add(this.txt_giatri);
+            this.Controls.Add(this.txt_loai);
+            this.Controls.Add(this.txt_ten);
+            this.Controls.Add(this.txt_macoupon);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "themcoupon";
+            this.Text = "themcoupon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_huy;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.DateTimePicker dt_ketthuc;
+        private System.Windows.Forms.DateTimePicker dt_ngaybatdau;
+        private System.Windows.Forms.TextBox txt_dontoithieu;
+        private System.Windows.Forms.TextBox txt_giatri;
+        private System.Windows.Forms.TextBox txt_loai;
+        private System.Windows.Forms.TextBox txt_ten;
+        private System.Windows.Forms.TextBox txt_macoupon;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
