c755351a5ba0e1b737f9eb15d73b38bc1bfc7199
tuanledstn-cpu <tuanledstn@gmail.com>
2026-09-21 02:51:10 +0700

    Them lich su giao dich va tich diem

---
+++ 06_BanHang/lichsugiaodich.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace SPORTSHOP._06_BanHang
+{
+    partial class lichsugiaodich
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_timkiem = new System.Windows.Forms.Button();
+            this.txt_timkiem = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgv_lichsu = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_quaylai = new System.Windows.Forms.Button();
+            this.btn_chitiet = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lichsu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(243, -74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "  LỊCH SỬ GIAO DỊCH  ";
+            // 
+            // btn_timkiem
+            // 
+            this.btn_timkiem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_timkiem.Location = new System.Drawing.Point(266, 88);
+            this.btn_timkiem.Name = "btn_timkiem";
+            this.btn_timkiem.Size = new System.Drawing.Size(135, 36);
+            this.btn_timkiem.TabIndex = 11;
+            this.btn_timkiem.Text = "🔎 Tìm Kiếm";
+            this.btn_timkiem.UseVisualStyleBackColor = true;
+            // 
+            // txt_timkiem
+            // 
+            this.txt_timkiem.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_timkiem.Location = new System.Drawing.Point(13, 91);
+            this.txt_timkiem.Name = "txt_timkiem";
+            this.txt_timkiem.Size = new System.Drawing.Size(247, 27);
+            this.txt_timkiem.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(243, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(318, 32);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "  LỊCH SỬ GIAO DỊCH  ";
+            // 
+            // dgv_lichsu
+            // 
+            this.dgv_lichsu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_lichsu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgv_lichsu.Location = new System.Drawing.Point(4, 130);
+            this.dgv_lichsu.Name = "dgv_lichsu";
+            this.dgv_lichsu.RowHeadersWidth = 51;
+            this.dgv_lichsu.RowTemplate.Height = 24;
+            this.dgv_lichsu.Size = new System.Drawing.Size(793, 363);
+            this.dgv_lichsu.TabIndex = 8;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mã đơn";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Ngày đặt";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Sản phẩm ";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 175;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Tổng tiền ";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Trạng thái";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // btn_quaylai
+            // 
+            this.btn_quaylai.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_quaylai.Location = new System.Drawing.Point(426, 572);
+            this.btn_quaylai.Name = "btn_quaylai";
+            this.btn_quaylai.Size = new System.Drawing.Size(135, 36);
+            this.btn_quaylai.TabIndex = 13;
+            this.btn_quaylai.Text = "Quay Lại";
+            this.btn_quaylai.UseVisualStyleBackColor = true;
+            // 
+            // btn_chitiet
+            // 
+            this.btn_chitiet.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_chitiet.Location = new System.Drawing.Point(169, 572);
+            this.btn_chitiet.Name = "btn_chitiet";
+            this.btn_chitiet.Size = new System.Drawing.Size(167, 36);
+            this.btn_chitiet.TabIndex = 12;
+            this.btn_chitiet.Text = "Xem Chi Tiết";
+            this.btn_chitiet.UseVisualStyleBackColor = true;
+            // 
+            // lichsugiaodich
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 619);
+            this.Controls.Add(this.btn_timkiem);
+            this.Controls.Add(this.txt_timkiem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgv_lichsu);
+            this.Controls.Add(this.btn_quaylai);
+            this.Controls.Add(this.btn_chitiet);
+            this.Controls.Add(this.label1);
+            this.Name = "lichsugiaodich";
+            this.Text = "lichsugiaodich";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lichsu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_timkiem;
+        private System.Windows.Forms.TextBox txt_timkiem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgv_lichsu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Button btn_quaylai;
+        private System.Windows.Forms.Button btn_chitiet;
+    }
+}
---
+++ 07_KhachHang/tichdiem.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace SPORTSHOP._07_KhachHang
+{
+    partial class tichdiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tichdiem));
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_mua = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.timerChiTieu = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblHang = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressChiTieu = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSoTien = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(9, 11);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 75);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "DIAMOND\r\n \r\n7999–9999k";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(10, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 75);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "    GOLD\r\n \r2999–5999k";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(526, 512);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(150, 100);
+            this.panel3.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(526, 374);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(150, 100);
+            this.panel2.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(14, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 75);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "SILVER\r\n \r\n999–2999k ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(32, 338);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(444, 418);
+            this.label3.TabIndex = 11;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // lbl_mua
+            // 
+            this.lbl_mua.AutoSize = true;
+            this.lbl_mua.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbl_mua.Location = new System.Drawing.Point(14, 147);
+            this.lbl_mua.Name = "lbl_mua";
+            this.lbl_mua.Size = new System.Drawing.Size(341, 23);
+            this.lbl_mua.TabIndex = 5;
+            this.lbl_mua.Text = "Mua thêm 999k để lên hạng DIAMOND";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Location = new System.Drawing.Point(526, 643);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(150, 100);
+            this.panel4.TabIndex = 14;
+            // 
+            // timerChiTieu
+            // 
+            this.timerChiTieu.Enabled = true;
+            this.timerChiTieu.Interval = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(13, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Chi tiêu quý hiện tại";
+            // 
+            // lblHang
+            // 
+            this.lblHang.AutoSize = true;
+            this.lblHang.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblHang.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblHang.Location = new System.Drawing.Point(533, 15);
+            this.lblHang.Name = "lblHang";
+            this.lblHang.Size = new System.Drawing.Size(83, 19);
+            this.lblHang.TabIndex = 2;
+            this.lblHang.Text = " BRONZE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(13, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Xin chào";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(15, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 29);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "←";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // progressChiTieu
+            // 
+            this.progressChiTieu.Location = new System.Drawing.Point(17, 91);
+            this.progressChiTieu.Maximum = 3000;
+            this.progressChiTieu.Name = "progressChiTieu";
+            this.progressChiTieu.Size = new System.Drawing.Size(598, 23);
+            this.progressChiTieu.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(196, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "HỘI VIÊN SPORTSHOP ";
+            // 
+            // lblSoTien
+            // 
+            this.lblSoTien.AutoSize = true;
+            this.lblSoTien.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblSoTien.Location = new System.Drawing.Point(534, 57);
+            this.lblSoTien.Name = "lblSoTien";
+            this.lblSoTien.Size = new System.Drawing.Size(67, 23);
+            this.lblSoTien.TabIndex = 4;
+            this.lblSoTien.Text = "0/999k";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lbl_mua);
+            this.panel1.Controls.Add(this.lblSoTien);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lblHang);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.progressChiTieu);
+            this.panel1.Location = new System.Drawing.Point(39, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(627, 216);
+            this.panel1.TabIndex = 8;
+            // 
+            // tichdiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(726, 787);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "tichdiem";
+            this.Text = "tichdiem";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_mua;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Timer timerChiTieu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblHang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressChiTieu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSoTien;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
